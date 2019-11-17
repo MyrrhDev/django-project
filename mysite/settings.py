4c80e1d14205d227c194c83707c9886fe20b83b4
Mayra Pastor Valdivia <mayra.pastor@est.fib.upc.edu>
2019-11-17 15:02:02 +0100

    Updated css

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-2y-34rd&l+e9k^id$d6b6b-)$jhpu@nuufazvb_^(6pg=6ur#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
